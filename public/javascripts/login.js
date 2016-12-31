30cff7fdf4fdcd1df4dcc1adf6d0503850beadee
ducnt114 <ducnt114@gmail.com>
2016-12-31 16:18:00 +0700

    Update

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -108,3 +108,38 @@ function renderGoogleButton() {
     'onfailure': loginWithGoogleonFailure
   });
 }
+
+function loginwup() {
+  var email = $('#idEmail').val();
+  var password = $('#idPassword').val();
+
+  if((isValidEmail(email) || isValidPhoneNumber(email)) && isValidPassword(password)){
+    var data = {
+      "type": "login",
+      "data": {
+        "email": email,
+        "password": password
+      }
+    }
+    var payload = JSON.stringify(data);
+    sock.send(payload);
+  }
+}
+
+function isValidPhoneNumber(text) {
+  var len = value.match(/\d/g).length;
+  return len === 10 || len === 11;
+}
+
+function isValidPassword(pwd) {
+  if (pwd != '') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isValidEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
